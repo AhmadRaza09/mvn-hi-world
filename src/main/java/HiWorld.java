fad6ed46d47d5cfde9a7bb2d6a5dc3cb30b6a10f
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-06-21 16:29:04 +0500

    first comit

---
+++ src/main/java/HiWorld.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World");
+        System.out.println(StringUtils.capitalize("hi world"));
+    }
+}
