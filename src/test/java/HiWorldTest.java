4b0d63e38a8ac145de784d9affee5d87c7bdfaea
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-07-01 15:24:02 +0500

    added test. closes #3.

---
+++ src/test/java/HiWorldTest.java
@@ -0,0 +1,9 @@
+public class HiWorldTest {
+    public void testHi(){
+        System.out.println("Hi from test");
+    }
+
+    public void testWorld(){
+        System.out.println("World from test");
+    }
+}
